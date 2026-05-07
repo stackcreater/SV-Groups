51fef6987abc7fb3a251f3d7999ea852bb85f460
Rahulk1409 <152679530+Rahulk1409@users.noreply.github.com>
2026-05-07 22:34:38 +0530

    Refactor firebase initialization for Vercel deployment

--- admin/src/lib/firebase-admin.ts
+++ admin/src/lib/firebase-admin.ts
@@ -2,8 +2,15 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
+    let credential;
+    if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+      credential = admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT));
+    } else {
+      credential = admin.credential.cert(require('../../firebase-service-account.json'));
+    }
+
     admin.initializeApp({
-      credential: admin.credential.cert(require('../../firebase-service-account.json')),
+      credential,
     });
   } catch (error) {
     console.log('Firebase admin initialization error', error);
